5e25458085acbccc185c22751f7ba29e44ddd6c4
Siti Halima <62000373+13020180070@users.noreply.github.com>
2020-04-12 23:27:15 +0800

    Add files via upload

---
+++ tugas 5/MultidimensiArray.java
@@ -0,0 +1,7 @@
+public class MultidimensiArray{  
+public static void main(String[] args) {
+        String[][] nama = {{"Sector COde","Harison"},{"Ganteng Banget","ganteng Juga"}};
+        System.out.println("dengan nama" + nama[0][1] + "orangnya" + nama[1][0]);
+        System.out.println(nama[1][1]);
+    }
+}
---
+++ tugas 5/operator.java
@@ -0,0 +1,10 @@
+
+public class operator{
+	public static void main (String args []){
+		
+		int a = 2, b=3, c=10,d=7,e=3;
+		int z = a+b+c+d+e;
+		System.out.print(z);
+	}
+ 
+}
